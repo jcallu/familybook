2b943c7ddb9d41570d480229ce41f6352eb36728
Jacinto Callu <jacintocallu@gmail.com>
2014-06-01 04:18:18 -0700

    mybook first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+    def index
+        redirect_to new_user_session_path unless user_signed_in?
+        #else
+        #@post = Post.new
+        #followees_ids = current_user.followees(User)
+        #get only the ids of the people current_user folllows
+        #followees_ids << current_user.id
+        #@activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User")
+        #end
+    end
+end
+
