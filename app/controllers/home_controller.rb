c23e3673bf2489b261772855288c051854b6ccf9
Jacinto Callu <jacintocallu@gmail.com>
2014-06-02 00:00:53 -0700

    Add unless else statement to user_sign_in?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
   def index
-    redirect_to new_user_session_path unless user_signed_in?
-    #else
+    unless user_signed_in?
+      redirect_to new_user_session_path 
+    else
     @post = Post.new
     followees_ids = current_user.followees(User)
     #get only the ids of the people current_user folllows
     followees_ids << current_user.id
     @activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User")
-    #end
+    end
   end
 end
 
