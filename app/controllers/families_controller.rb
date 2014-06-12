e80aec0cdf376dbd2932204fb2e839512c581742
Jacinto Callu <jacintocallu@gmail.com>
2014-06-12 15:37:17 -0700

    Add current_user's family list

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -7,19 +7,24 @@ class FamiliesController < ApplicationController
     @family = Family.new(family_params)
     @family.owner_id = @family.admin_id = current_user.id
     @family.short_name = family_params['name'].gsub(" ","_").downcase
-    @family.save
-    
     @family_membership = FamilyMembership.new
     @family_membership.user_id = current_user.id
     @family_membership.family_id = @family.id
-    @family_membership.save
-
-    redirect_to families_path
-  end 
+    respond_to do |format|
+      if @family_membership.save && @family.save
+        format.html { redirect_to families_path, notice: 'Family was successfully created.' }
+        format.json { render :index, status: :ok, location: families_path }
+      else
+        format.html { render :edit }
+        format.json { render json: @family.errors, status: :unprocessable_entity }
+      end
+    end 
+  end
   
   def index
+    @family = Family.new
     @families = Family.all
-    #redirect_to new_family_path if current_user.family_memberships.empty?
+    @my_families = current_user.family_memberships.collect{|r| r.family}
   end
 
   private
